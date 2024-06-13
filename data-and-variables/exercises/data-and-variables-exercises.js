9220d270c9cad1a6d4145490193db02e92e2968f
metracy <matthew.etracy@gmail.com>
2024-06-13 18:53:01 -0500

    Updated with info about mars and moon trip

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,29 @@
 // Declare and assign the variables below
-
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof shuttleName)
+console.log(typeof shuttleSpeedMph)
+console.log(typeof distanceToMarsKm)
+console.log(typeof distanceToMoonKm)
+console.log(typeof milesPerKm)
 // Calculate a space mission below
+let distanceToMarsMiles = distanceToMarsKm * milesPerKm
+let hoursToMars = distanceToMarsMiles / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
 
 // Print the results of the space mission calculations below
 
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.");
+
 // Calculate a trip to the moon below
+let distanceToMoonMiles = distanceToMoonKm * milesPerKm;
+let hoursToMoon = distanceToMoonMiles / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+
 
 // Print the results of the trip to the moon below
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");

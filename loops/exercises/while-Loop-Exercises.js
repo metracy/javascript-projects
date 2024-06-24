4031466e48cf7f7937ea7bdfbd8bf232f8c7468f
metracy <matthew.etracy@gmail.com>
2024-06-23 19:19:26 -0500

    while-loop-exercises works when I plug I run from terminal with `node while-Loop-Exercises.js` but debug console hangs?

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,4 +1,8 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+const input = require('readline-sync');
+let fuelLevel = 0;
+let numAstronauts = 0;
+let altitude = 0;
 
 
 
@@ -7,19 +11,34 @@
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
-
-
-
+while (fuelLevel < 5000 || isNaN(fuelLevel) || fuelLevel > 30000) {
+    fuelLevel = input.question("Enter the starting fuel level (between 5000 and 30000): ");
+}
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
+
+while (numAstronauts <= 0 || isNaN(numAstronauts) || numAstronauts > 7) {
+  numAstronauts = input.question("Enter number of astronaughts between 1 (minimum) and 7 (maximum): ");
+}
   
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
 
-
+while (fuelLevel-100*numAstronauts >= 0) {
+  altitude += 50;
+  fuelLevel -= 100*numAstronauts;
+}
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+let output = `The shuttle gained an altitude of ${altitude} km.`;
+
+if (altitude >= 2000) {
+output += " Orbit achieved!";
+}
+else{
+  output += " Failed to reach orbit."
+}
+console.log(output)
